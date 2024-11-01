8565d8ca86841318c974fbd96177a0dde7cf0d2a
unknown <kot.timurka911@gmail.com>
2024-11-01 12:07:10 +0400

    Add existing file

--- frst_file.py
+++ frst_file.py
@@ -1,6 +1,6 @@
 class User:
     def __init__(self, user_id, name, age, email):
-        self.user-id = user_id
+        self.user_id = user_id
         self.name = name
         self.age = age
         self.email = email
