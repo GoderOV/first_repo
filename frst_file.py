1fd74fe5f272fba614d94ef0335113f58899b4fe
unknown <kot.timurka911@gmail.com>
2024-11-25 20:36:20 +0400

    new

--- frst_file.py
+++ frst_file.py
@@ -1,13 +1,36 @@
-class User:
-    def __init__(self, user_id, name, age, email):
-        self.user_id = user_id
-        self.name = name
-        self.age = age
-        self.email = email
+import logging  
+import requests  
+from aiogram import Bot, Dispatcher, types    
+from aiogram.utils import executor
 
-def get_user_info(user: User) -> str:
-    return f'Возврат пользователя {user.name} - {user.age}, ' \
-        f'а email - {user.email}'
-        
-user_1: User = User(42, "Vasiliy", 23, "vasya_pupkin@pochta.ru")
-print(get_user_info(user_1))
+API_TOKEN = 'Y7290191892:AAFmxpVWNtL5cYA-pight6m_f72qCSISKs4'
+
+# Настройка логгирования  
+logging.basicConfig(level=logging.INFO)
+
+# Инициализация бота и диспетчера  
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+# Обработка команды /recipe  
+@dp.message_handler(commands=['recipe'])
+async def send_recipe_link(message: types.Message):
+    recipe_name = message.get_args()
+    if not recipe_name:
+        await message.answer("Пожалуйста, укажите название рецепта после /recipe. Например: /recipe пицца")
+        return
+
+    response = requests.get(f'https://api.edamam.com/search?q={recipe_name}&app_id=Y7290191892:AAFmxpVWNtL5cYA-pight6m_f72qCSISKs4&app_key=Y7290191892:AAFmxpVWNtL5cYA-pight6m_f72qCSISKs4')
+    if response.status_code == 200:
+        data = response.json()
+        if data.get('hits'):
+            recipe_url = data['hits'][0]['recipe']['url']
+            await message.answer(f"Вот ссылка на рецепт '{recipe_name}': {recipe_url}")
+        else:
+            await message.answer("Извините, не удалось найти рецепт. Попробуйте другой запрос.")
+    else:
+        await message.answer("Произошла ошибка при запросе. Пожалуйста, попробуйте позже.")
+
+# Запуск бота  
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
